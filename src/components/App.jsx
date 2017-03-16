274b94848b78462f5ce24404874fcd422104c695
j- <j@skeoh.com>
2017-03-17 10:49:56 +1100

    Add some body text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import DataTransferContainer from './DataTransferContainer';
 const App = () => (
 	<div>
 		<h1>Data Transfer</h1>
+		<p>Copy+paste or click+drag into this window.</p>
+		<p><strong>Latest at top</strong></p>
 		<DataTransferContainer />
 	</div>
 );

c7fa604db576692326a4543440f2abc55e1c0815
j- <j@skeoh.com>
2017-03-17 11:03:39 +1100

    Add dummy input field for mobile devices

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,10 @@ const App = () => (
 	<div>
 		<h1>Data Transfer</h1>
 		<p>Copy+paste or click+drag into this window.</p>
+		<p>
+			<label htmlFor="dummy-input">Use this text field to paste from a mobile device</label><br />
+			<input type="text" id="dummy-input" />
+		</p>
 		<p><strong>Latest at top</strong></p>
 		<DataTransferContainer />
 	</div>

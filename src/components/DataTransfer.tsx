135a12cced11e21c0dedae34686b265f8349c403
j- <j@skeoh.com>
2017-07-05 21:06:31 +1000

    Ignore validation error

--- src/components/DataTransfer.tsx
+++ src/components/DataTransfer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StoredDataTransfer } from '../types';
 
-const replacer = (key: string, value: any) => {
+const replacer = (key: string, value: any) => { // tslint:disable-line
 	if (key.charAt(0) === '_') {
 		return undefined;
 	}

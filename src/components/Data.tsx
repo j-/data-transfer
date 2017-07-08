15ec3dfb3a1d61af32598a3b00142801671b28c8
j- <j@skeoh.com>
2017-07-08 19:58:28 +1000

    Add copy button

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './Data.css';
 
+const copy = require('clipboard-copy');
+
 export interface Props {
 	type: string;
 	data: string;
@@ -12,13 +14,28 @@ const Empty = () => (
 	</em>
 );
 
+const Copy = ({ data }: { data: string }) => (
+	<button
+		type="button"
+		className="Data-copy"
+		onClick={() => copy(data)}
+	>
+		(Copy)
+	</button>
+);
+
 export default class Data extends React.Component<Props> {
 	render () {
 		const { type, data } = this.props;
 		return (
 			<div className="Data">
-				<div className="Data-type">{type}</div>
-				<div className="Data-data">{data || <Empty />}</div>
+				<div className="Data-type">
+					<Copy data={data} />
+					{type}
+				</div>
+				<div className="Data-data">
+					{data || <Empty />}
+				</div>
 			</div>
 		);
 	}

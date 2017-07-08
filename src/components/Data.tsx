79d8d19991ac69b396dcb86fa8ec359f3c6f8e77
j- <j@skeoh.com>
2017-07-08 19:16:46 +1000

    Show missing data as "Empty"

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -6,13 +6,19 @@ export interface Props {
 	data: string;
 }
 
+const Empty = () => (
+	<em className="Data-empty">
+		Empty
+	</em>
+);
+
 export default class Data extends React.Component<Props> {
 	render () {
 		const { type, data } = this.props;
 		return (
 			<div className="Data">
 				<div className="Data-type">{type}</div>
-				<div className="Data-data">{data}</div>
+				<div className="Data-data">{data || <Empty />}</div>
 			</div>
 		);
 	}

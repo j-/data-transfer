78105231758ef5b19e91513fafe35c0414c1c32d
j- <j@skeoh.com>
2019-01-30 13:26:44 +1100

    Prevent textarea from receiving input

--- src/components/PasteInput.tsx
+++ src/components/PasteInput.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import './PasteInput.css';
 
+const cancel: React.EventHandler<React.SyntheticEvent<HTMLElement>> = (e) => e.preventDefault();
+
 const PasteInput = () => (
 	<div className="PasteInput">
 		<textarea
 			className="PasteInput-textarea form-control d-block"
 			type="text"
 			placeholder="Mobile devices: long press and select 'Paste'\nDesktops/laptops: right click and select 'Paste'"
+			value=""
+			onInput={cancel}
+			onChange={cancel}
 		/>
 	</div>
 );

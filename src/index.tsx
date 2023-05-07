156613b9a6defe3ae84b48aedefdec3ef535528d
j- <j@skeoh.com>
2023-05-07 15:38:50 +1000

    Reduce document font size

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
 render(<App />, rootElement);

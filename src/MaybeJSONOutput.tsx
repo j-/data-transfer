644e7be0d9e72bda9f4e4017c2744a9ca4546ff7
j- <j@skeoh.com>
2023-10-09 15:35:48 +1100

    Add missing file

---
+++ src/MaybeJSONOutput.tsx
@@ -0,0 +1,37 @@
+import isPromise from 'is-promise';
+import JSONOutput from './JSONOutput';
+import { useEffect, useState } from 'react';
+
+export type MaybeJSONOutputProps<T> = {
+  value: T | Promise<T> | PromiseLike<T>;
+};
+
+function MaybeJSONOutput<T>({ value }: MaybeJSONOutputProps<T>) {
+  const initialResult = isPromise(value) ? <>&hellip;</> : <JSONOutput value={value} />;
+  const [contents, setContents] = useState(initialResult);
+
+  useEffect(() => {
+    if (!isPromise(value)) return;
+    let isMounted = true;
+
+    Promise.resolve(value)
+      .then((result) => {
+        if (isMounted) {
+          setContents(<JSONOutput value={result} />);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setContents(<>Error: {err.message}</>);
+        }
+      })
+
+    return () => {
+      isMounted = false;
+    };
+  }, [value, setContents]);
+
+  return contents;
+};
+
+export default MaybeJSONOutput;

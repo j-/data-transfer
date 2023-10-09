341375416ac98d300530c9456e5cde48ce9d4fd5
j- <j@skeoh.com>
2023-10-09 15:15:27 +1100

    Add "Open in new tab" to file list item reports

--- src/ReportFileListItem.tsx
+++ src/ReportFileListItem.tsx
@@ -8,14 +8,39 @@ type Props = {
   file: File;
 };
 
-const ReportFileListItem: React.FC<Props> = ({ path, index, file }) => (
-  <Report key={`ReportFileListItem-${index}`}>
-    <DefinitionPairInline label={`${path}.lastModified`} value={file.lastModified} />
-    <DefinitionPairInline label={`${path}.name`} value={file.name} />
-    <DefinitionPairInline label={`${path}.webkitRelativePath`} value={file.webkitRelativePath} />
-    <DefinitionPairInline label={`${path}.size`} value={file.size} />
-    <DefinitionPairInline label={`${path}.type`} value={file.type} />
-  </Report>
-);
+const ReportFileListItem: React.FC<Props> = ({ path, index, file }) => {
+  const [objectURL, setObjectURL] = React.useState('');
+
+  const handleClickOpen = React.useCallback(() => {
+    window.open(objectURL, '_blank');
+  }, [objectURL]);
+
+  React.useEffect(() => {
+    const url = URL.createObjectURL(file);
+    setObjectURL(url);
+    return () => URL.revokeObjectURL(url);
+  }, [file]);
+
+  return (
+    <Report key={`ReportFileListItem-${index}`}>
+      <DefinitionPairInline label={`${path}.lastModified`} value={file.lastModified} />
+      <DefinitionPairInline label={`${path}.name`} value={file.name} />
+      <DefinitionPairInline label={`${path}.webkitRelativePath`} value={file.webkitRelativePath} />
+      <DefinitionPairInline label={`${path}.size`} value={file.size} />
+      <DefinitionPairInline label={`${path}.type`} value={file.type} />
+      {file.type !== '' ? (
+        <div className="ReportFileListItem-actions flex gap-2">
+          <button
+            className="btn btn-neutral btn-sm normal-case"
+            type="button"
+            onClick={handleClickOpen}
+          >
+            Open in new tab
+          </button>
+        </div>
+      ) : null}
+    </Report>
+  );
+};
 
 export default ReportFileListItem;

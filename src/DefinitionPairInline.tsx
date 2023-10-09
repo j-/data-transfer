38e82d0759b4f2c83504c538c875eb818e8b7c0e
j- <j@skeoh.com>
2023-10-09 15:20:02 +1100

    Make inline definition pair handle promises

--- src/DefinitionPairInline.tsx
+++ src/DefinitionPairInline.tsx
@@ -1,4 +1,6 @@
+import isPromise from 'is-promise';
 import JSONOutput from './JSONOutput';
+import { useEffect, useState } from 'react';
 
 export type DefinitionPairInlineProps = {
   label: string;
@@ -6,16 +8,42 @@ export type DefinitionPairInlineProps = {
   defaultExpanded?: boolean;
 };
 
-const DefinitionPairInline: React.FC<DefinitionPairInlineProps> = ({ label, value }) => (
-  <div>
-    <div className="inline-block text-gray-600 max-w-[50%] truncate font-mono after:content-['\27f9'] after:mx-2 after:text-gray-400">
-      {label}
-    </div>
+const DefinitionPairInline: React.FC<DefinitionPairInlineProps> = ({ label, value }) => {
+  const initialResult = isPromise(value) ? <>&hellip;</> : <JSONOutput value={value} />;
+  const [contents, setContents] = useState(initialResult);
+  // const [result, setResult] = useState(value);
 
-    <div className="inline-block truncate">
-      <JSONOutput value={value} />
+  useEffect(() => {
+    if (!isPromise(value)) return;
+    let isMounted = true;
+    Promise.resolve(value)
+      .then((result) => {
+        // setResult(result);
+        if (isMounted) {
+          setContents(<JSONOutput value={result} />);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setContents(<>Error: {err.message}</>);
+        }
+      })
+    return () => {
+      isMounted = false;
+    };
+  }, [value, setContents]);
+
+  return (
+    <div>
+      <div className="inline-block text-gray-600 max-w-[50%] truncate font-mono after:content-['\27f9'] after:mx-2 after:text-gray-400">
+        {label}
+      </div>
+
+      <div className="inline-block truncate">
+        {contents}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default DefinitionPairInline;
